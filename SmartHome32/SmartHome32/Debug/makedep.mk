428bd176e074d733537e80983a19e185b6b4188c
TeymurPC <teymur.hesenzade@hotmail.com>
2018-01-23 21:36:50 +0400

    Add files via upload

---
+++ SmartHome32/SmartHome32/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+lcd\lcd_hd44780_avr.c
+
+main.c
+
+tux\dnslkup.c
+
+tux\enc28j60.c
+
+tux\ip_arp_udp_tcp.c
+
+tux\websrv_help_functions.c
+
